aaaff25e5995ceb3e9de8c5538601451ce362767
Masdapa <92660769+Masdapa@users.noreply.github.com>
2022-11-29 14:57:37 +0700

    Update emojimix.js

--- plugins/converter/emojimix.js
+++ plugins/converter/emojimix.js
@@ -15,17 +15,14 @@ exports.run = {
          let [emo1, emo2] = text.split`+`
          if (!emo1 || !emo2) return client.reply(m.chat, Func.texted('bold', `🚩 Berikan 2 emoticon untuk di mix.`), m)
          let json = await Api.emojimix(emo1 + '_' + emo2)
-         if (!json.status) return client.reply(m.chat, Func.texted('bold', `🚩 Emoticon tidak bisa di mix.`), m)
-         await client.sendSticker(m.chat, img, m, {
+         return await client.sendSticker(m.chat, img, m, {
             packname: exif.sk_pack,
             author: exif.sk_author,
-            categories: [emo1, emo2]
          })
       } catch (e) {
+         console.log(e)
          return client.reply(m.chat, Func.jsonFormat(e), m)
       }
    },
    error: false,
-   cache: true,
-   location: __filename
 }
